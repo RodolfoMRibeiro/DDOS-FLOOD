39a911c0891b2da8b303a013864342c8563be819
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-06-17 12:05:18 -0300

    fix: fix invalid header unifier

--- ddos/spider.go
+++ ddos/spider.go
@@ -8,21 +8,21 @@ import (
 
 // MULTIPLE HEADERS TO FAKE DIFFERENT REQUESTS
 var acceptall = []string{
-	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-	"Accept-Encoding: gzip, deflate\r\n",
-	"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-	"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
-	"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-	"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-	"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-	"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-	"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
-	"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;Accept-Language: en-US,en;q=0.5;Accept-Encoding: gzip, deflate;",
+	"Accept-Encoding: gzip, deflate;",
+	"Accept-Language: en-US,en;q=0.5;Accept-Encoding: gzip, deflate;",
+	"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8;Accept-Language: en-US,en;q=0.5;Accept-Charset: iso-8859-1;Accept-Encoding: gzip;",
+	"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5;Accept-Charset: iso-8859-1;",
+	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;Accept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1;Accept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1;Accept-Charset: utf-8, iso-8859-1;q=0.5;",
+	"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*;Accept-Language: en-US,en;q=0.5;",
+	"Accept: text/html, application/xhtml+xml, image/jxr, */*;Accept-Encoding: gzip;Accept-Charset: utf-8, iso-8859-1;q=0.5;Accept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1;",
+	"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1;Accept-Encoding: gzip;Accept-Language: en-US,en;q=0.5;Accept-Charset: utf-8, iso-8859-1;q=0.5;",
+	"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8;Accept-Language: en-US,en;q=0.5;",
+	"Accept-Charset: utf-8, iso-8859-1;q=0.5;Accept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1;",
 	"Accept: text/html, application/xhtml+xml",
-	"Accept-Language: en-US,en;q=0.5\r\n",
-	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
-	"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+	"Accept-Language: en-US,en;q=0.5;",
+	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;Accept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1;",
+	"Accept: text/plain;q=0.8,image/png,*/*;q=0.5;Accept-Charset: iso-8859-1;",
 }
 
 // PLATFORMS, OS AND BROWSERS
