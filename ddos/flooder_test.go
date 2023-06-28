07c6d4e556bc4a3cc984f0434eb0e8c23aa0319a
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-06-28 19:29:11 -0300

    test: add test to ddos package

---
+++ ddos/flooder_test.go
@@ -0,0 +1,75 @@
+package ddos
+
+import (
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type FlooderTestSuite struct {
+	suite.Suite
+	flooder Flooder
+}
+
+type MockClient struct {
+	mock.Mock
+}
+
+func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
+	args := m.Called(req)
+	return args.Get(0).(*http.Response), args.Error(1)
+}
+
+func (suite *FlooderTestSuite) SetupTest() {
+	suite.flooder = NewFlooder("http://example.com", 1, 1)
+}
+
+func (suite *FlooderTestSuite) TestSetWorkerAmount() {
+	assert := assert.New(suite.T())
+
+	amountField := reflect.ValueOf(suite.flooder).Elem().FieldByName("workerAmount")
+	assert.False(amountField.IsZero(), "Unable to get workerAmount field")
+
+	assert.Equal(uint64(1), amountField.Uint())
+
+	suite.flooder.SetWorkerAmount(5)
+
+	assert.Equal(uint64(5), amountField.Uint())
+}
+
+func (suite *FlooderTestSuite) TestSetDuration() {
+	start := time.Now()
+
+	suite.flooder.SetDuration(2)
+
+	suite.flooder.Start()
+
+	elapsed := time.Since(start)
+
+	assert.InDelta(suite.T(), 2*time.Second, elapsed, float64(1000*time.Millisecond))
+}
+
+func (suite *FlooderTestSuite) TestSendRequest() {
+	mockClient := new(MockClient)
+	flooderStruct := suite.flooder.(*flooder)
+	flooderStruct.client = mockClient
+
+	// Mocking the http client response
+	mockClient.On("Do", mock.Anything).Maybe().Return(&http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(strings.NewReader("OK")),
+	}, nil)
+
+	mockClient.AssertExpectations(suite.T())
+}
+
+func TestFlooderTestSuite(t *testing.T) {
+	suite.Run(t, new(FlooderTestSuite))
+}
